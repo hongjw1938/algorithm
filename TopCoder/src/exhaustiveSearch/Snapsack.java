7cd2cdb60945a645671e529a09a2b4fd40a3a579
hongjw1991 <sighingtree@naver.com>
2018-06-02 21:33:13 +0900

    snapsack improved way plus

--- TopCoder/src/exhaustiveSearch/Snapsack.java
+++ TopCoder/src/exhaustiveSearch/Snapsack.java
@@ -21,7 +21,7 @@ public class Snapsack {
 	
 	public void dfs(int n, int w, int v) {
 		
-		//만약 weight가 10 초과이면 return
+		//留뚯빟 weight媛� 10 珥덇낵�씠硫� return
 		if(w > 10) return;
 		
 		ans = Math.max(ans, v);
@@ -32,5 +32,13 @@ public class Snapsack {
 		System.out.println("dfs(" + (n+1) + ", " + (w+weightSnap[n]) + ", " + (v+valueSnap[n]) + ")");
 		dfs(n+1, w + weightSnap[n], v + valueSnap[n]);
 		
+	}
+	
+	//다른 방식
+	public int knapsack(int n, int w) {
+		
+		if(w > 10) return -1;
+		if(n > weightSnap.length) return 0;
+		return Math.max(knapsack(n + 1, w), knapsack(n + 1, w + weightSnap[n]) + valueSnap[n]);
 	}
 }
