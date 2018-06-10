4b79c439c7415cde888694857f7682258850f92a
hongjw1991 <sighingtree@naver.com>
2018-06-10 11:59:33 +0900

    Corporation Salary memorization

--- TopCoder/src/exhaustiveSearch/CorporationSalary.java
+++ TopCoder/src/exhaustiveSearch/CorporationSalary.java
@@ -69,5 +69,47 @@ public class CorporationSalary {
 		
 		
 	}
+	
+	//다른 코드 해석
+	public long totalS(String[] relations) {
+		sals = new long[relations.length];
+		
+		
+		//반환하기 위한 최종값
+		long total = 0;
+		
+		for(int i=0; i < relations.length; i++) {
+			total += getSal(i, relations);
+		}
+		
+		return total;
+	}
+	
+	private long getSal(int i, String[] relations) {
+		
+		//초기화된 상태라면
+		if(sals[i] == 0) {
+			//해당 사원의 월급
+			long sal = 0;
+			
+			//해당 사원의 관계도
+			String relation = relations[i];
+			
+			for(int j=0; j < relations.length; j++) {
+				
+				//각 관계를 조사하여 해당 관계가 Y인 경우
+				if(relation.charAt(i) == 'Y') {
+					//재귀호출하고 NNNN까지 찾아 해당 값들을 리턴받아 계속 더함
+					sal += getSal(j, relations);
+				}
+			}
+			
+			//재귀호출로 넘어가지 않은 경우 NNNN과 같이 모두 N이므로 가장 아래 직급. 월급은 1
+			if(sal == 0) sal = 1;
+				
+			sals[i] = sal;
+		}
+		return sals[i];
+	}
 
 }
